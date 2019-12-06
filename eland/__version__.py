206276c5faf92af94207cadd478cd32763ef4dcd
stevedodson <stevedodson@users.noreply.github.com>
2019-12-06 09:44:05 +0000

    Adding Apache 2 copyright header to all .py files (#86)

--- eland/__version__.py
+++ eland/__version__.py
@@ -1,3 +1,17 @@
+#  Copyright 2019 Elasticsearch BV
+#
+#      Licensed under the Apache License, Version 2.0 (the "License");
+#      you may not use this file except in compliance with the License.
+#      You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#      Unless required by applicable law or agreed to in writing, software
+#      distributed under the License is distributed on an "AS IS" BASIS,
+#      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#      See the License for the specific language governing permissions and
+#      limitations under the License.
+
 __title__ = 'eland'
 __description__ = 'Python elasticsearch client to analyse, explore and manipulate data that resides in elasticsearch.'
 __url__ = 'https://github.com/elastic/eland'
