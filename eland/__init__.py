03582b9f5eeab73426b4bd007218efdb2e9d0434
Daniel Mesejo-León <mesejoleon@gmail.com>
2020-03-30 14:45:04 +0200

    Import __version__ and other metadata by name

--- eland/__init__.py
+++ eland/__init__.py
@@ -14,7 +14,16 @@
 
 # flake8: noqa
 
-from eland._version import *
+from eland._version import (
+    __title__,
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __maintainer__,
+    __maintainer_email__,
+)
 from eland.common import *
 from eland.client import *
 from eland.filter import *
