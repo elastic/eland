6ca41585e94f9f9d2458fcc5a8233d8d20d1e86f
Seth Michael Larson <seth.larson@elastic.co>
2020-05-14 10:07:10 -0500

    Upgrade to elasticsearch-py v7.7

--- eland/_version.py
+++ eland/_version.py
@@ -5,7 +5,7 @@
 __title__ = "eland"
 __description__ = "Python elasticsearch client to analyse, explore and manipulate data that resides in elasticsearch."
 __url__ = "https://github.com/elastic/eland"
-__version__ = "7.6.0a5"
+__version__ = "7.7.0a1"
 __author__ = "Steve Dodson"
 __author_email__ = "steve.dodson@elastic.co"
 __maintainer__ = "Seth Michael Larson"
