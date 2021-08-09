b0c8434c06b3425b88aafd0e54f7305b783240dd
Seth Michael Larson <seth.larson@elastic.co>
2021-08-09 09:11:57 -0500

    Release 7.14.0b1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     keywords="elastic eland pandas python",
     packages=find_packages(include=["eland", "eland.*"]),
     install_requires=[
-        "elasticsearch>=7.14,<8",
+        "elasticsearch>=7.11,<8",
         "pandas>=1.2,<1.4",
         "matplotlib",
         "numpy",
