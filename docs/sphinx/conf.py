17c1c2e9c7c9099d53b8e972e71576915cea2c6a
Seth Michael Larson <seth.larson@elastic.co>
2023-06-15 09:51:14 -0500

    Switch to the 'Furo' Sphinx theme

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -115,12 +115,7 @@ exclude_patterns = ["**.ipynb_checkpoints"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "pydata_sphinx_theme"
-html_theme_options = {
-    "external_links": [],
-    "github_url": "https://github.com/elastic/eland",
-    "twitter_url": "https://twitter.com/elastic",
-}
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
