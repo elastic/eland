63ca50e56fae691e888cc86a95c7a458d99e6abe
Bart Broere <mail@bartbroere.eu>
2023-09-07 17:05:52 +0200

    Remove deprecated check_less_precise in unit test and replace with rtol

--- tests/series/test_arithmetics_pytest.py
+++ tests/series/test_arithmetics_pytest.py
@@ -74,7 +74,8 @@ class TestSeriesArithmetics(TestData):
             ed_series.__class__ = ModifiedElandSeries
 
             assert_pandas_eland_series_equal(
-                pd_series, ed_series, check_less_precise=True
+                pd_series, ed_series,
+                rtol=1e-3  # previously known as check_less_precise
             )
 
     def test_ecommerce_series_invalid_div(self):
