630c81fb5da4b627aff62c3ee2fe34f4c226576b
Bart Broere <mail@bartbroere.eu>
2024-05-21 11:39:35 +0200

    Fix missing method str.removeprefix in Python 3.8

--- eland/ml/pytorch/transformers.py
+++ eland/ml/pytorch/transformers.py
@@ -1059,5 +1059,10 @@ def elasticsearch_model_id(model_id: str) -> str:
 
     id = re.sub(r"[\s\\/]", "__", model_id).lower()[-64:]
     if id.startswith("__"):
-        id = id.removeprefix("__")
+        # This check is only needed as long as Eland supports Python 3.8
+        # str.removeprefix was introduced in Python 3.9
+        if sys.version_info > (3, 9, 0):
+            id = id.removeprefix("__")
+        else:
+            id = id[2:]
     return id
