30cc6553de779a91eb5011b6b9ce4df7aa71c8ac
Quentin Pradet <quentin.pradet@elastic.co>
2024-10-01 17:54:07 +0400

    Allow scikit-learn 1.5 to address CVE-2024-5206

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ with open(path.join(here, "README.md"), "r", "utf-8") as f:
 
 extras = {
     "xgboost": ["xgboost>=0.90,<2"],
-    "scikit-learn": ["scikit-learn>=1.3,<1.4"],
+    "scikit-learn": ["scikit-learn>=1.3,<1.6"],
     "lightgbm": ["lightgbm>=2,<4"],
     "pytorch": [
         "requests<3",
