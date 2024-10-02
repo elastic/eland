48dbb5bbd26c819894c2bf0d25970e67a904348f
Quentin Pradet <quentin.pradet@elastic.co>
2024-10-02 09:51:06 +0400

    Only build Linux images

--- .buildkite/build-docker-images.sh
+++ .buildkite/build-docker-images.sh
@@ -3,7 +3,9 @@
 set -eo pipefail
 export LC_ALL=en_US.UTF-8
 
-# Create builder that supports QEMU emulation (needed for linux/arm64)
-docker buildx rm --force eland-multiarch-builder || true
-docker buildx create --name eland-multiarch-builder --bootstrap --use
-docker buildx build --file Dockerfile.wolfi --load --platform $PLATFORM .
+echo "--- Building the Wolfi image"
+# Building the linux/arm64 image takes about one hour on Buildkite, which is too slow
+docker build --file Dockerfile.wolfi .
+
+echo "--- Building the public image"
+docker build .
