417e4babd5b1a2db86a4fc4c4587497c376d8a9b
Quentin Pradet <quentin.pradet@elastic.co>
2024-10-02 09:10:06 +0400

    Parallelize docker build
    It's currently the slowest step.

--- .buildkite/build-docker-images.sh
+++ .buildkite/build-docker-images.sh
@@ -6,8 +6,4 @@ export LC_ALL=en_US.UTF-8
 # Create builder that supports QEMU emulation (needed for linux/arm64)
 docker buildx rm --force eland-multiarch-builder || true
 docker buildx create --name eland-multiarch-builder --bootstrap --use
-
-for platform in linux/amd64 linux/arm64; do
-  echo "--- Building $platform"
-  docker buildx build --file Dockerfile.wolfi --load --platform $platform .
-done
+docker buildx build --file Dockerfile.wolfi --load --platform $PLATFORM .
