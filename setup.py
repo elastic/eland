73b7f2af0fd4ba2997064a8ddbb7d8d018ab6e07
Quentin Pradet <quentin.pradet@elastic.co>
2025-12-04 16:33:03 +0400

    Try without sentence-transformers

--- setup.py
+++ setup.py
@@ -62,7 +62,8 @@ extras = {
         "requests<3",
         "torch==2.7.1",
         "tqdm",
-        "sentence-transformers>=5.0.0,<6.0.0",
+        # not currently compatible with transformers v5
+        # "sentence-transformers>=5.0.0,<6.0.0",
         # sentencepiece is a required dependency for the slow tokenizers
         # https://huggingface.co/transformers/v4.4.2/migration.html#sentencepiece-is-removed-from-the-required-dependencies
         "transformers[sentencepiece]>=5.0.0rc0",
